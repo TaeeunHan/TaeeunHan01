5442b7fec55ad1e7a0507fe4b4963631b333c2dc
TaeeunHan <eineblume1@gmail.com>
2023-01-04 22:22:28 +0900

    한태은_포트폴리오

--- js/main.js
+++ js/main.js
@@ -10,79 +10,117 @@ btnCall.onclick = function (e) {
 };
 
 //slider
-const frame = document.querySelector('#visual .inner');
-const boxs = frame.querySelectorAll('article');
-const prev = document.querySelector('.prev');
-const next = document.querySelector('.next');
-let SliderEnableClick = true;
+const frame = document.querySelector('#visual');
+const panels = frame.querySelectorAll('.panel li');
+const btns = frame.querySelectorAll('.btns li');
+const btnPlay = frame.querySelector('.fa-play');
+const btnPause = frame.querySelector('.fa-pause');
+const bar = frame.querySelector('.bar');
+const len = panels.length - 1;
+const interval = 5000;
+let num = 0;
+let timer = null;
 
-init();
+//로딩시 startRolling함수 호출해서 패널 자동롤링 시작
+startRolling();
 
-next.addEventListener('click', (e) => {
-	if (!SliderEnableClick) return;
-	SliderEnableClick = false;
-	nextSlide(e);
+//각 버튼 클릭시 활성화 함수실행 및 stopRoliing으로 롤링기능 정지
+btns.forEach((btn, idx) => {
+	btn.addEventListener('click', () => {
+		activation(idx);
+		stopRolling();
+	});
 });
 
-prev.addEventListener('click', (e) => {
-	if (!SliderEnableClick) return;
-	SliderEnableClick = false;
-	prevSlide(e);
-});
-function init() {
-	frame.prepend(frame.lastElementChild);
-	frame.prepend(frame.lastElementChild);
-	boxs[0].classList.add('on');
+//재생, 정지 버튼 클릭시 startRolling, stopRolling함수 호출
+btnPlay.addEventListener('click', startRolling);
+btnPause.addEventListener('click', stopRolling);
+
+//버튼 , 패널 활성화함수
+function activation(index) {
+	//인수로 전달받은 버튼과 패널만 활성화
+	for (const el of panels) el.classList.remove('on');
+	for (const el of btns) el.classList.remove('on');
+	panels[index].classList.add('on');
+	btns[index].classList.add('on');
+	//인수로 전달받은 순번으로 전역 활성화 순번 갱신
+	num = index;
+	//패널 활성화시 진행바의 넓이값을 0%로 초기화
+	bar.style.width = '0%';
 }
 
-function nextSlide(e) {
-	e.preventDefault();
-	frame.append(frame.firstElementChild);
-	activation1();
+//롤링함수
+function rolling() {
+	num < len ? num++ : (num = 0);
+	//활성화 순번에 맞게 패널, 버튼 활성화
+	activation(num);
+	//진행바 모션 시작
+	progress();
 }
 
-function prevSlide(e) {
-	e.preventDefault();
-	frame.prepend(frame.lastElementChild);
-	activation1();
+//롤링 시작 함수
+function startRolling() {
+	//진행바 보임처리
+	bar.style.display = 'block';
+	//setTimeout으로 progress함수를 강제 동기화 해서
+	//bar보임처리 이후에 모션실행
+	setTimeout(progress, 0);
+
+	//활성화 함수 호출하면서
+	//다시 롤링시작
+	activation(num);
+	timer = setInterval(rolling, interval);
+
+	//재생버튼 활성화
+	btnPlay.classList.add('on');
+	btnPause.classList.remove('on');
 }
 
-function activation1() {
-	setTimeout(() => {
-		const boxs = frame.querySelectorAll('article');
-		for (const el of boxs) el.classList.remove('on');
-		boxs[2].classList.add('on');
-		SliderEnableClick = true;
-	}, 500);
+//롤링 정지 함수
+function stopRolling() {
+	//진행바 숨김처리
+	bar.style.display = 'none';
+	//자동롤링 중지
+	clearInterval(timer);
+	//정지버튼 활성화
+	btnPause.classList.add('on');
+	btnPlay.classList.remove('on');
 }
+
+//진행바 모션 함수
+function progress() {
+	new Anime(bar, {
+		prop: 'width',
+		value: '100%',
+		duration: interval,
+	});
+}
+
+//scroll
 
 //tab
-const main = document.querySelector('main');
-const btns = main.querySelectorAll('ul li');
+const main = document.querySelector('#tab main');
+const Tbtns = main.querySelectorAll('ul li');
 const tabBoxs = main.querySelectorAll('section article');
 let speed = convertSpeed(tabBoxs[0]);
 let TabEnableClick = true;
 
-btns.forEach((el, ind) => {
+Tbtns.forEach((el, ind) => {
 	el.addEventListener('click', (e) => {
 		e.preventDefault();
 
 		if (TabEnableClick) {
 			TabEnableClick = false;
-			activation(btns, ind);
-			activation(tabBoxs, ind);
-			new Anim(main, {
-				prop: 'height',
-				value: matchHT(ind),
-				duration: speed,
-			});
+			activation2(Tbtns, ind);
+			activation2(tabBoxs, ind);
 		}
 	});
 });
 
-function activation(arr, index) {
+function activation2(arr, index) {
 	for (let tabBox of arr) tabBox.classList.remove('on');
 	arr[index].classList.add('on');
+	console.log(arr);
 
 	setTimeout(() => {
 		TabEnableClick = true;
